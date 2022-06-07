2e6000760849639f4b28137123a49c875bb856df
CarloDeLeon25 <contact.carlo.deleon@gmail.com>
2022-06-07 21:12:34 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        monsterrat: ["Montserrat", "sans-serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      fontSize: {
+        extrabold: "800",
+      },
+      colors: {
+        violet: "#9600EF",
+      },
+    },
+  },
+  plugins: [],
+};

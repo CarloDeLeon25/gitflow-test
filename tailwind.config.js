a6a5e8c333f53d2dbe336b68f4b21ac48f5f6fdd
CarloDeLeon25 <contact.carlo.deleon@gmail.com>
2022-06-13 21:37:24 +0800

    First commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
       },
       colors: {
         violet: "#9600EF",
+        yellow: "#ffbe00",
+        pink: "#ff0064",
+        blue: "#3c79ff",
+        white: "#FBFDF7",
       },
     },
   },
